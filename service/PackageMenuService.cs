3ae61476bf781eb241e7bd1646e3c68985acc145
ShinAruu22 <ardiansyahfikri276@gmail.com>
2025-12-14 11:23:54 +0700

    Coba yang peckage

--- service/PackageMenuService.cs
+++ service/PackageMenuService.cs
@@ -8,10 +8,48 @@ public class PackageMenuService
     }
     // ==================================
     
-    public PackageMenu CreateMenu(PackageMenu menuRequest){ return null;}
-    public PackageMenu FindAnyMenu(string id){return null;} 
+    public PackageMenu CreateMenu(PackageMenu menuRequest)
+    {
+        PackageMenu package = new();
+        package.menu_type = menuRequest.menu_type;
+        package.name = menuRequest.name;
+        package.time = menuRequest.time;
+        package.price = menuRequest.price;
+
+        repository.DB.Add(package);
+
+        return package;
+    }
+    public PackageMenu FindAnyMenu(string id)
+    {
+        foreach (PackageMenu package in repository.DB)
+        {
+            if(package.id == id)
+            {
+                return package;
+            }
+        }
+        return null;
+    } 
     public List<PackageMenu> FindAllMenu(){return repository.DB;}
-    public PackageMenu UpdateMenu(PackageMenu menuRequest){return null;}
-    public void DeleteMenu(string id){}
+    public PackageMenu UpdateMenu(PackageMenu menuRequest)
+    {
+        PackageMenu findMenu = FindAnyMenu(menuRequest.id);
+        int targetIndex = repository.DB.FindIndex(x => x.id == findMenu.id);
+
+        PackageMenu packageResponse = findMenu;
+
+        return packageResponse;
+    }
+    public void DeleteMenu(string id)
+    {
+        foreach (PackageMenu findMenu in repository.DB)
+        {
+            if(findMenu.id == id)
+            {
+                repository.DB.Remove(findMenu);
+            }
+        }
+    }
 
 }
