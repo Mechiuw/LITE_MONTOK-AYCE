07a3daed65211a188c9355272d437e599262c9d3
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2025-12-16 17:12:15 +0700

    [WIP] Wip on transaction, must be done quick

--- model/entity/Session.cs
+++ model/entity/Session.cs
@@ -4,7 +4,8 @@ public class Session
     public int total_person;
     public List<string> id_package;
     public Timer timer_session;
+    public List<string> penalties;
     public bool open_status;
     
     
-}
+} // lanjutin ini session dan transaction
--- service/TransactionService.cs
+++ service/TransactionService.cs
@@ -1,4 +1,26 @@
 public class TransactionService
 {
-    
+    private readonly TransactionRepository repository;
+    private readonly SessionRepository sessionRepository;
+
+    public TransactionService(SessionRepository _sessionRepository,TransactionRepository _repository)
+    {
+        this.repository = _repository;
+        this.sessionRepository = _sessionRepository;
+    }
+
+    public Transaction Create(Transaction transactionRequest)
+    {
+        Session validSession = new();
+        foreach (Session session in sessionRepository.DB)
+        {
+            if(transactionRequest.id_session == session.id)
+            {
+                validSession = session;
+            }
+        }
+        // lanjutin ini session dan transaction
+
+        return new();
+    }
 }
